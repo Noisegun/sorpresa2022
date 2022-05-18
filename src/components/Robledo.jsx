53c65af6cce4bda52a6a42bb9f773bec7f3e3332
Noisegun <j.maria.cimadevilla@gmail.com>
2022-05-19 01:29:12 +0200

    Update Robledo.jsx

--- src/components/Robledo.jsx
+++ src/components/Robledo.jsx
@@ -8,7 +8,7 @@ function Robledo()
            Otro lugar de los escondidos de Madrid donde hacer mil fotos y disfrutar de un lugar único.
         </p>
         <p style={{paddingLeft: "2em", paddingRight: "2em", textAlign: "justify", marginBottom: "2em"}}>
-           Creo que sobran las palabras, de este plan sabes tú mucho más que yo...
+           Este plan contiene curvas pero también fotos alucinantes... Como la que ves aquí.
         </p>
         <img alt="Robledo" src="https://s0.wklcdn.com/image_39/1183554/7605168/4351149Master.jpg" width={"700px"} style={{paddingLeft: "2em", paddingBottom: "1em"}} />
         <p style={{paddingLeft: "2em", paddingRight: "2em", textAlign: "justify", marginTop: "2em"}}>
